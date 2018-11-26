781be2749fe7f615050d7c954042af50e2d27482
github username <github email address>
2018-11-25 18:41:45 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,17 @@
-def test
- puts "test1"
+class Book
+
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+    def initialize(title) 
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
 end
 
-test
+Book.new("And then there was none")
